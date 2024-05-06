0a1ae76205e4f87c519819674d8b28d77ad8ccf7
Kingcoder44 <113973573+Kingcoder44@users.noreply.github.com>
2024-05-06 23:15:09 +0530

    Add files via upload

---
+++ Variables_Test.kt
@@ -0,0 +1,11 @@
+package com.example.first
+
+fun main()
+{
+    val constantNumber = 45
+    var mutableString = "Test"
+    println(constantNumber)
+    println( mutableString)
+    mutableString = "Changed"
+    println(mutableString)
+}
---
+++ str.kt
@@ -0,0 +1,9 @@
+package com.example.first
+//show implementaion of string functions
+fun main()
+{
+    var name = "A quick brown fox jumped over a lazy dog"
+    println(name.lowercase())
+    println(name.uppercase())
+    println(name + " BLEGH " )
+}
